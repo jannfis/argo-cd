64daef8a3c6b53c6a84b0dc98e076f6c0e41ffcd
jannfis <jann@mistrust.net>
2020-05-28 14:06:46 +0200

    test

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -15,15 +15,14 @@ swagger version
 jq --version
 
 PROJECT_ROOT=$(cd $(dirname ${BASH_SOURCE})/..; pwd)
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${PROJECT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 PATH="${PROJECT_ROOT}/dist:${PATH}"
 GRPC_GW_PATH=$(go list -mod=mod -m -f "{{.Dir}}" github.com/grpc-ecosystem/grpc-gateway)
 
 . ${PROJECT_ROOT}/hack/versions.sh
 
 # protbuf tooling required to build .proto files from go annotations from k8s-like api types
-go build -i -o dist/go-to-protobuf ./vendor/k8s.io/code-generator/cmd/go-to-protobuf
-go build -i -o dist/protoc-gen-gogo ./vendor/k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo
+go build -i -o dist/go-to-protobuf k8s.io/code-generator/cmd/go-to-protobuf
+go build -i -o dist/protoc-gen-gogo k8s.io/code-generator/cmd/go-to-protobuf/protoc-gen-gogo
 
 # Generate pkg/apis/<group>/<apiversion>/(generated.proto,generated.pb.go)
 # NOTE: any dependencies of our types to the k8s.io apimachinery types should be added to the
@@ -58,13 +57,13 @@ ${PROJECT_ROOT}/dist/go-to-protobuf \
 #GOPROTOBINARY=gofast
 # 3. protoc-gen-gogofast - faster code generation and gogo extensions and flexibility in controlling
 # the generated go code (e.g. customizing field names, nullable fields)
-go build -i -o dist/protoc-gen-gogofast ./vendor/github.com/gogo/protobuf/protoc-gen-gogofast
+go build -i -o dist/protoc-gen-gogofast github.com/gogo/protobuf/protoc-gen-gogofast
 GOPROTOBINARY=gogofast
 
 # protoc-gen-grpc-gateway is used to build <service>.pb.gw.go files from from .proto files
-go build -i -o dist/protoc-gen-grpc-gateway ./vendor/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go build -i -o dist/protoc-gen-grpc-gateway github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 # protoc-gen-swagger is used to build swagger.json
-go build -i -o dist/protoc-gen-swagger ./vendor/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go build -i -o dist/protoc-gen-swagger github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
 # Generate server/<service>/(<service>.pb.go|<service>.pb.gw.go)
 PROTO_FILES=$(find $PROJECT_ROOT \( -name "*.proto" -and -path '*/server/*' -or -path '*/reposerver/*' -and -name "*.proto" \) | sort)
@@ -74,7 +73,7 @@ for i in ${PROTO_FILES}; do
     protoc \
         -I${PROJECT_ROOT} \
         -I/usr/local/include \
-        -I${PROJECT_ROOT}/vendor \
+        -I./vendor \
 	-I${GOPATH}/src \
 	-I${GOPATH}/pkg/mod \
         -I${GOOGLE_PROTO_API_PATH} \

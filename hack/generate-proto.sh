6d5893cf30823ddd8d3ff05b2e49f2615bf846af
jannfis <jann@mistrust.net>
2020-05-28 14:24:35 +0200

    test

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -45,7 +45,7 @@ ${PROJECT_ROOT}/dist/go-to-protobuf \
     --go-header-file=${PROJECT_ROOT}/hack/custom-boilerplate.go.txt \
     --packages=$(IFS=, ; echo "${PACKAGES[*]}") \
     --apimachinery-packages=$(IFS=, ; echo "${APIMACHINERY_PKGS[*]}") \
-    --proto-import=./vendor
+    --proto-import=${PROJECT_ROOT}/vendor
 
 # Either protoc-gen-go, protoc-gen-gofast, or protoc-gen-gogofast can be used to build
 # server/*/<service>.pb.go from .proto files. golang/protobuf and gogo/protobuf can be used
@@ -75,6 +75,7 @@ for i in ${PROTO_FILES}; do
         -I${PROJECT_ROOT} \
         -I/usr/local/include \
         -I./vendor \
+	-I${PROJECT_ROOT}/vendor \
         -I$GOPATH/src \
         -I${GOOGLE_PROTO_API_PATH} \
         -I${GOGO_PROTOBUF_PATH} \

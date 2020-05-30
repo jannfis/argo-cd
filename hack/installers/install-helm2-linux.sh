502bbc81200de79fc5bd6d3b1724a6ae698cb899
jannfis <jann@mistrust.net>
2020-05-30 18:44:13 +0200

    Caching

--- hack/installers/install-helm2-linux.sh
+++ hack/installers/install-helm2-linux.sh
@@ -3,7 +3,7 @@ set -eux -o pipefail
 
 . $(dirname $0)/../tool-versions.sh
 
-[ -e $DOWNLOADS/helm2.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm2.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-${helm2_version}-linux-amd64.tar.gz
+[ -e $DOWNLOADS/helm2.tar.gz ] || curl -sLf --retry 3 -o $DOWNLOADS/helm2.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v${helm2_version}-linux-amd64.tar.gz
 mkdir -p /tmp/helm2 && tar -C /tmp/helm2 -xf $DOWNLOADS/helm2.tar.gz
 cp /tmp/helm2/linux-amd64/helm $BIN/helm2
 helm2 version --client

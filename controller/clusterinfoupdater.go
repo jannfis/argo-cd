c4d79b8a1764ca1422aea27710b1968ac67b7e57
jannfis <jann@mistrust.net>
2023-02-11 15:16:34 -0500

    feat: Controller to actively monitor clusters
    Signed-off-by: jannfis <jann@mistrust.net>

--- controller/clusterinfoupdater.go
+++ controller/clusterinfoupdater.go
@@ -2,15 +2,17 @@ package controller
 
 import (
 	"context"
-	"time"
 	"fmt"
+	"time"
+
 	"github.com/argoproj/gitops-engine/pkg/cache"
 	"github.com/argoproj/gitops-engine/pkg/utils/kube"
+	"github.com/argoproj/gitops-engine/pkg/utils/tracing"
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 
-	"github.com/argoproj/argo-cd/v2/controller/metrics"
+	statecache "github.com/argoproj/argo-cd/v2/controller/cache"
 	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v2/pkg/client/listers/application/v1alpha1"
 	"github.com/argoproj/argo-cd/v2/util/argo"
@@ -20,10 +22,11 @@ import (
 
 const (
 	secretUpdateInterval = 10 * time.Second
+	pingInterval         = 10 * time.Second // TODO: Make this interval configurable and provide reasonable default
 )
 
 type clusterInfoUpdater struct {
-	infoSource    metrics.HasClustersInfo
+	infoSource    statecache.LiveStateCache
 	db            db.ArgoDB
 	appLister     v1alpha1.ApplicationNamespaceLister
 	cache         *appstatecache.Cache
@@ -33,7 +36,7 @@ type clusterInfoUpdater struct {
 }
 
 func NewClusterInfoUpdater(
-	infoSource metrics.HasClustersInfo,
+	infoSource statecache.LiveStateCache,
 	db db.ArgoDB,
 	appLister v1alpha1.ApplicationNamespaceLister,
 	cache *appstatecache.Cache,
@@ -46,16 +49,102 @@ func NewClusterInfoUpdater(
 
 func (c *clusterInfoUpdater) Run(ctx context.Context) {
 	c.updateClusters()
-	ticker := time.NewTicker(secretUpdateInterval)
+	updateTicker := time.NewTicker(secretUpdateInterval)
+	pingTicker := time.NewTicker(pingInterval)
+	pingChan := make(chan bool)
+OUTER:
 	for {
 		select {
 		case <-ctx.Done():
-			ticker.Stop()
-			break
-		case <-ticker.C:
+			updateTicker.Stop()
+			pingTicker.Stop()
+			break OUTER
+		case <-updateTicker.C:
 			c.updateClusters()
+		case <-pingTicker.C:
+			// Pinging the clusters may take some time. We stop the ticker,
+			// so that not multiple pings will run at the same time, and we
+			// reset the ticker once the ping has completed.
+			pingTicker.Stop()
+			// We spawn the ping process in its own goroutine, so it does
+			// not interfere with the cluster update interval.
+			go c.pingClusters(pingChan)
+		case <-pingChan:
+			pingTicker.Reset(pingInterval)
 		}
 	}
+}
+
+// pingClusters sends a version request to remote clusters' APIs. If this
+// fails, it will invalidate the corresponding cluster cache so the cluster
+// will be marked as failed, if the error does not recover meanwhile.
+func (c *clusterInfoUpdater) pingClusters(ch chan bool) {
+	defer func() {
+		ch <- true
+	}()
+	clusters, err := c.getClustersFiltered()
+	if err != nil {
+		log.Errorf("Could not retrieve list of clusters for cluster ping: %v", err)
+		return
+	}
+	_ = kube.RunAllAsync(len(clusters), func(i int) error {
+		for _, ci := range c.infoSource.GetClustersInfo() {
+			if ci.Server == clusters[i].Server {
+				c.pingCluster(clusters[i], ci)
+				return nil
+			}
+		}
+		return nil
+	})
+}
+
+// pingCluster will perform a ping-style request to the given cluster, and will
+// execute a cluster cache invalidation on a failing cluster, and a re-sync on
+// a recovering one.
+func (c *clusterInfoUpdater) pingCluster(cluster appv1.Cluster, info cache.ClusterInfo) {
+	logCtx := log.WithField("server", cluster.Server)
+	logCtx.Debugf("Pinging cluster to check availability")
+
+	k := kube.KubectlCmd{Tracer: tracing.NopTracer{}}
+	// TODO: Do we want configurable retry for the version request?
+	// TODO: Do we want configurable timeout for the version reqest?
+	v, err := k.GetServerVersion(cluster.RESTConfig())
+	if err != nil {
+		// Ping error and last sync error is nil means we just lost the cluster
+		if info.SyncError == nil {
+			logCtx.Infof("Cluster became unavailable. Reason: '%v')", err)
+			if err := c.invalidateClusterCache(cluster); err != nil {
+				logCtx.Infof("Failed to invalidate cluster cache: %v", err)
+				return
+			}
+		} else {
+			logCtx.Debugf("Cluster still unavailable")
+		}
+	} else {
+		// Ping success and last sync error is set means cluster just recovered
+		if info.SyncError != nil {
+			logCtx.Infof("Cluster became available again after %vs", time.Since(*info.LastCacheSyncTime).Seconds())
+			if err := c.invalidateClusterCache(cluster); err != nil {
+				logCtx.Info("Failed to mark cluster as available: %v", err)
+			}
+		} else {
+			logCtx.Debugf("Cluster available. Version: '%v'", v)
+		}
+	}
+
+	c.updateClusterInfo(cluster, &info)
+}
+
+// invalidateClusterCache invalidates a cluster cache and subsequentially
+// ensures it is synced again.
+func (c *clusterInfoUpdater) invalidateClusterCache(cluster appv1.Cluster) error {
+	cache, err := c.infoSource.GetClusterCache(cluster.Server)
+	if err != nil {
+		return err
+	}
+	cache.Invalidate()
+	err = cache.EnsureSynced()
+	return err
 }
 
 func (c *clusterInfoUpdater) updateClusters() {
@@ -65,29 +154,19 @@ func (c *clusterInfoUpdater) updateClusters() {
 		info := clustersInfo[i]
 		infoByServer[info.Server] = &info
 	}
-	clusters, err := c.db.ListClusters(context.Background())
+	clusters, err := c.getClustersFiltered()
 	if err != nil {
 		log.Warnf("Failed to save clusters info: %v", err)
 		return
 	}
-	var clustersFiltered []appv1.Cluster
-	if c.clusterFilter == nil {
-		clustersFiltered = clusters.Items
-	} else {
-		for i := range clusters.Items {
-			if c.clusterFilter(&clusters.Items[i]) {
-				clustersFiltered = append(clustersFiltered, clusters.Items[i])
-			}
-		}
-	}
-	_ = kube.RunAllAsync(len(clustersFiltered), func(i int) error {
-		cluster := clustersFiltered[i]
+	_ = kube.RunAllAsync(len(clusters), func(i int) error {
+		cluster := clusters[i]
 		if err := c.updateClusterInfo(cluster, infoByServer[cluster.Server]); err != nil {
 			log.Warnf("Failed to save clusters info: %v", err)
 		}
 		return nil
 	})
-	log.Debugf("Successfully saved info of %d clusters", len(clustersFiltered))
+	log.Debugf("Successfully saved info of %d clusters", len(clusters))
 }
 
 func (c *clusterInfoUpdater) updateClusterInfo(cluster appv1.Cluster, info *cache.ClusterInfo) error {
@@ -138,4 +217,24 @@ func (c *clusterInfoUpdater) updateClusterInfo(cluster appv1.Cluster, info *cach
 	}
 
 	return c.cache.SetClusterInfo(cluster.Server, &clusterInfo)
+}
+
+// getClustersFiltered returns a list of clusters from the configuration that
+// this controller is responsible for.
+func (c *clusterInfoUpdater) getClustersFiltered() ([]appv1.Cluster, error) {
+	clusters, err := c.db.ListClusters(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	var clustersFiltered []appv1.Cluster
+	if c.clusterFilter == nil {
+		clustersFiltered = clusters.Items
+	} else {
+		for i := range clusters.Items {
+			if c.clusterFilter(&clusters.Items[i]) {
+				clustersFiltered = append(clustersFiltered, clusters.Items[i])
+			}
+		}
+	}
+	return clustersFiltered, nil
 }
